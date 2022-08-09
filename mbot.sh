4938d049c146665b19b2462a7e21926e3cd1b015
abhishekhbhootwala <88439521+abhishekhbhootwala@users.noreply.github.com>
2022-08-09 19:35:31 +0600

    Update mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,4 +1,4 @@
 cd m*t*t
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
-git clone https://github.com/rootedcyber93/mirror-leech-telegram-bot;cd m*t*t;pip3 install --no-cache-dir -r requirements.txt;bash start.sh
+git clone https://github.com/abhishekhbhootwala/Deegram2;cd m*t*t;pip3 install --no-cache-dir -r requirements.txt;bash start.sh
